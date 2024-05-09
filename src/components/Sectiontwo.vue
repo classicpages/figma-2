14516600f04695161884f0cba43fc9a985daaa15
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-05-09 03:28:28 +0000

    Sectiontwo setup

--- src/components/Sectiontwo.vue
+++ src/components/Sectiontwo.vue
@@ -73,30 +73,30 @@
 
     <div class="com">
       <div class="nic">
-        <img src="/src/assets/images/Rectangle 377.png" alt="" />
-        <p>Fashion Landing</p>
-        <a class="view" href="">view details</a>
+        <img src="/src/assets/images/Rectangle 377.png" class="img" alt="" />
       </div>
+      <p>Fashion Landing</p>
+      <a class="view" href="">view details</a>
 
-      <div class="nices">
-        <img src="/src/assets/images/Rectangle 378.png" alt="" />
-        <p>Furniture Shop</p>
-        <a class="view" href="">view details</a>
+      <div class="nic">
+        <img src="/src/assets/images/Rectangle 378.png" class="img" alt="" />
       </div>
+      <p>Furniture Shop</p>
+      <a class="view" href="">view details</a>
     </div>
 
     <div class="com">
-      <div class="nice">
-        <img src="/src/assets/images/Rectangle 379.png" alt="" />
-        <p>Health & Fitness</p>
-        <a class="view" href="">view details</a>
+      <div class="nic">
+        <img src="/src/assets/images/Rectangle 379.png" class="img" alt="" />
       </div>
+      <p>Health & Fitness</p>
+      <a class="view" href="">view details</a>
 
-      <div class="nice">
-        <img src="/src/assets/images/Rectangle 380.png" alt="" />
-        <p>IT Solution Provider</p>
-        <a class="view" href="">view details</a>
+      <div class="nic">
+        <img src="/src/assets/images/Rectangle 380.png" class="img" alt="" />
       </div>
+      <p>IT Solution Provider</p>
+      <a class="view" href="">view details</a>
     </div>
 
     <div>
@@ -139,10 +139,122 @@
         </div>
       </div>
     </div>
+    <div>
+    </div>
 
-    div
+    <div>
+        <p class="price">Pricing</p>
+        <p class="pricing">Pricing Policy</p>
+    </div>
+
+    <div class="money">
+
+        <div class="for camp">
+            <p class="fors">For Starter</p>
+            <p class="pris">$1499</p>
+            <div class="feed">
+                <p><img src="/src/assets/images/Vector.png" /> Feedback Categorization</p>
+                <p><img src="/src/assets/images/Vector.png" /> Features prioritization</p>
+                <p><img src="/src/assets/images/Vector.png" /> Real-time collaboration</p>
+                <p><img src="/src/assets/images/Vector.png" /> Feedback loop notifications</p> 
+                <p><img src="/src/assets/images/Vector.png" /> Essential dev tools integrations</p> 
+            </div>
+
+            <button class="buts">Request Demo</button>
+
+        </div>
+
+
+            <div class="for comp">
+                <p class="fors">For Teams</p>
+                <p class="pris">$2999</p>
+    <div class="feed">
+        <p>. Feedback Categorization</p>
+                <p><img src="/src/assets/images/Vector.png" /> Features prioritization</p>
+                <p><img src="/src/assets/images/Vector.png" /> Real-time collaboration</p>
+                <p><img src="/src/assets/images/Vector.png" /> Feedback loop notifications</p> 
+                <p><img src="/src/assets/images/Vector.png" /> Essential dev tools integrations</p> 
+    </div>
+
+    <button class="but">Request Demo</button>
+
+            </div>
+    
+            <div class="for camp">
+                <p class="fors">For Company</p>
+                <p class="pris">Custom</p>
+    <div class="feed">
+
+        <p><img src="/src/assets/images/Vector.png" /> Feedback Categorization</p>
+        <p><img src="/src/assets/images/Vector.png" /> Feedback loop notifications</p> 
+        <p><img src="/src/assets/images/Vector.png" /> Essential dev tools integrations</p>
+    </div> 
+
+    <button class="buts">Contact Us</button>
+            </div>
+
+    </div>
+
+    <div class="footer">
+        <p class="need"> Need a little more <br>
+            home to grow?</p>
+
+            <button class="but">Contact Us</button>
+
+            <div class="foot">
+                <div class="foots">
+                    <p class="dev">Web.Dev</p>
+                
+                    <p>You’ll find your next home loan <br>
+                         valu you prefer.</p>
+
+                         <div class="image">
+                        <div><img class="images" src="../assets/images/Icon.png" /></div>
+                           <div><img class="images" src="../assets/images/Icon (1).png" /></div>
+                            <div><img class="images" src="../assets/images/Icon (2).png" /></div>
+                        </div>
+                </div>
+
+                <div class="foots">
+                    <p class="devs">Resources</p>
+
+                    <p>Our Agents</p>
+                    <p>Member Stories</p>
+                    <p>Video</p>
+                    <p>Free trial</p>
+                </div>
+
+
+                <div class="foots">
+                    <p class="devs">Company</p>
+
+                    <p>Patnerships</p>
+                    <p>Terms of use</p>
+                    <p>Privacy</p>
+                    <p>Sitemap</p>
+                </div>
+              
+                <div class="foots">
+                    <p class="devs">Get in touch</p>
+                    
+                    <input class="input" type="text" placeholder="Enter your mail">
+                   
+                </div>
+                
+            </div>
+
+            <div class="copy">
+                <p>Copyright 2021 ©Web.Dev  All rights reserved.</p>
+            </div>
+
+
+    </div>
+
+
   </section>
 </template>
+
+
 
 <style scoped>
 .icon {
@@ -151,15 +263,137 @@
   color: #a1a89d;
 }
 
+.foots {
+    margin-left: 150px;
+}
+
+.dev {
+    font-size: 25px;
+    font-weight: 400;
+}
+
+.images {
+    padding-left: 20px;
+}
+
+.image {
+    display: flex;
+}
+
+.devs {
+    font-size: 17px;
+    height: 25px;
+    font-weight: 600;
+}
+
+.copy {
+    border: 1px solid #ffff;
+    line-height: 16px;
+    margin-top: 30px;
+}
+
+.input {
+    margin-top: 30px;
+    width: 140px;
+    font-size: 12px;
+    font-weight: 500;
+    height: 30px;
+    padding-left: 15px;
+    border: 1px solid #06F0FF4D;
+}
+
+.foot {
+    display: flex;
+    margin-top: 100px;
+}
+
+.for {
+    width: 400px;
+    height: 400px;
+    border-radius: 5px;
+    border: 1px solid #EDEDED;
+}
+
+.need {
+    font-size: 45px;
+    height: 100px;
+    
+}
+
+.camp {
+    margin-left: 20px;
+    margin-right: 20px;
+    width: 310px;
+    margin-left: 70px;
+    border: 1px solid #EDEDED
+
+}
+
+.comp {
+    background-color: #FFFFFF;
+    margin-left: 30px;
+    margin-right: 30px;
+    width: 310px;
+}
+.fors {
+    font-size: 30px;
+    
+    
+}
+
+.but {
+    background-color:  #D82CD1F0;
+    border: 1px solid #D82CD1F0;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 30px;
+    padding-right: 30px;
+    border-radius: 3px;
+} 
+
+.buts {
+    border: 1px solid black;
+    background-color:  #06F0FF4D;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 30px;
+    padding-right: 30px;
+    border-radius: 6px;
+    margin-bottom: 50px;
+}
+
+
+.feed {
+    text-align: left;
+    margin-left: 30px;
+    font-weight: 500;
+}
+
+.pris {
+    font-size: 40px;
+}
+
+.money {
+    display: flex;
+}
+
+.price {
+    margin-top: 100px;
+}
+
+.pricing {
+    margin-top: 60px;
+    font-size: 45px;
+}
+
 .sub {
   border: 1px solid #fff;
   background-color: #f2f2f2;
-  width: 130px;
-  height: 280px;
-  top: 400px;
+  width: 600px;
   left: 135px;
-  margin-left: 20px;
+  margin-left: 150px;
   display: flex;
+    
 }
 
 .test {
@@ -172,36 +406,42 @@
 }
 
 .testimonials {
-  border: 1px solid #f2f2f2;
-  width: 900px;
-  background-color: #ffffff;
+  border: 1px solid #fff;
+  width: 800px;
+  height: 350px;
+  background-color: #f2f2f2;
   display: flex;
   border-radius: 6px;
-  padding-left: 10px;
-  padding-right: 10px;
+  padding-left: 60px;
+  padding-right: 60px;
+  
+
+
 }
 
 .comment {
-  width: 330px;
-  height: 22px;
-  top: 4110px;
-  left: 135px;
+  width: 892px;
+  top: 4007px;
+  left: 274px;
   border: 1px solid #fff;
-  background-color: #f2f2f2;
+  background-color: #fff;
   height: 280px;
-  left: 135px;
   margin-left: 10px;
   margin-right: 10px;
   padding-right: 10px;
+  margin-top: 30px;
+  margin-bottom: 50px;
+  
 }
 
 .view {
   color: #49ad09;
   font-size: 14px;
+  float: right;
 }
 
 .img {
-  width: 100px;
+  width: 300px;
 }
 
 .port {
@@ -212,7 +452,6 @@
 }
 .nic {
   background-color: white;
-  border: 1px solid white;
   margin-right: 20px;
   padding: 10px;
 }
@@ -234,7 +473,12 @@
 }
 
 .web {
-  font-size: 25px;
+  font-size: 14px;
+  font-weight: 600;
+  text-align: left;
+  margin-left: 30px;
+  padding-top: 40px;
+  padding-bottom: 20px;
 }
 
 .real {
